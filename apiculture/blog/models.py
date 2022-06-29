26b8e4100f774ff7189c24b7530edf2d550faafc
V_Robin <valentinrobin1@gmail.com>
2022-06-30 01:23:18 +0200

    front started

--- apiculture/blog/models.py
+++ apiculture/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profil_picture = models.ImageField(upload_to=get_userpic_path, blank=True,
+                                       null=True,
+                                       validators=[validate_picture])
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
